73461cd7d063613badfb964a7c9a05a5eb48cf74
Dan Bareille <dan.bareillegmail.com>
2022-08-05 10:18:02 +0200

    Ajouts fichier .js pour affichage cards canapé

--- front/js/script.js
+++ front/js/script.js
@@ -1,11 +1,37 @@
-function itemListe() {
-    fetch('http://localhost:3000/api/products')
-        .then(reponse => reponse.json())
-        .then(data => {
+let cardsFetch = function () {
+
+   fetch('http://localhost:3000/api/products')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      let productSection = document.getElementById("items");
+
+      for (i = 1; i < data.length; i++) {
+        const productCard = `
+          <a href="./product.html?id=${data[i]._id}">
+            <article>
+              <img src="${data[i].imageUrl}" alt="${data[i].altTxt}"/>
+              <h3 class="productName">${data[i].name}</h3>
+              <p class="productDescription">${data[i].description}</p>
+            </article>
+          </a>
+        `;
+        productSection.innerHTML += productCard;
+
+      }
+    });
+};
+cardsFetch();   
+
+    
+    
+    
+ 
+    
 
 
-        })
-}
+
 
 
 
