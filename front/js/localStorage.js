2014cf04ce7c166e965603e876a0aa6cf71ca897
Dan Bareille <dan.bareillegmail.com>
2022-08-10 16:08:10 +0200

    Amélioration code dans product.js

--- front/js/localStorage.js
+++ front/js/localStorage.js
@@ -1,15 +1,37 @@
 const localStorageAccess = localStorage
 
-function getAllProductsFromLocalStorage(){
+function getAllProductsFromLocalStorage(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart))
 
 }
 
-function updateLocalStorage(products){
-
+function updateLocalStorage(products) {
+    return JSON.parse(localStorage.getItem("cart"))
 }
 
-function addToCart(id, color, quantity){
+function addToCart(id, color, quantity) {
+    let cart = updateLocalStorage()
+    cart.push(id, color, quantity)
+    getAllProductsFromLocalStorage(cart)
 
 }
 
 // get set
+
+// const localStorageAccess = localStorage
+
+// function getAllProductsFromLocalStorage(cart) {
+//     localStorage.setItem("cart", JSON.stringify(cart))
+
+// }
+
+// function updateLocalStorage(products) {
+//     return JSON.parse(localStorage.getItem("cart"))
+// }
+
+// function addToCart(id, color, quantity) {
+//     let cart = updateLocalStorage()
+//     cart.push(id, color, quantity)
+//     getAllProductsFromLocalStorage(cart)
+
+// }
--- front/js/product.js
+++ front/js/product.js
@@ -1,47 +1,50 @@
 let str = "http://localhost:3000/api/products";
-    let url = new URL(location);
-    console.log(url);
-    let urlParams = (url).searchParams
-    console.log(urlParams);
+let url = new URL(location);
+console.log(url);
+let urlParams = (url).searchParams
+console.log(urlParams);
 
-    let productId = urlParams.get("id")
-    console.log(productId);
+let productId = urlParams.get("id")
+console.log(productId);
 
-    fetch('http://localhost:3000/api/products/'+ productId)
-    .then((response) => response.json())
-    .then((data) => {
+fetch('http://localhost:3000/api/products/' + productId)
+  .then((response) => response.json())
+  .then((data) => {
 
-        let imgContainer = document.querySelector(".item__img")
-        let productImg = document.createElement("img")
-        productImg.src = data.imageUrl
-        productImg.alt = data.altTxt 
-        imgContainer.appendChild(productImg)
+    let imgContainer = document.querySelector(".item__img")
+    let productImg = document.createElement("img")
+    productImg.src = data.imageUrl
+    productImg.alt = data.altTxt
+    productImg.alt = data.altTxt
+    productImg.alt = data.altTxt
+    imgContainer.appendChild(productImg)
 
-        let pageTitle = document.querySelector("#title")
-        pageTitle.innerHTML = data.name
+    let pageTitle = document.querySelector("#title")
+    pageTitle.innerHTML = data.name
 
-        let pageSelect = document.querySelector("#colors")
-        for (let color of data.colors) {
-            console.log(color);
+    let priceProduct = document.querySelector("#price")
+    priceProduct.innerHTML = data.price
 
-            let option = document.createElement("option")
-            option.value = color
-            option.innerHTML = color
-            pageSelect.appendChild(option)
+    let descriptionProduct = document.querySelector("#description")
+    descriptionProduct.innerHTML = data.description
 
-            
-            let descriptionProduct = document.querySelector("#description")  
-            descriptionProduct.innerHTML = data.description
+    let colorSelect = document.querySelector("#colors")
+    for (let color of data.colors) {
+      console.log(color);
 
-            let priceProduct = document.querySelector("#price")
-            priceProduct.innerHTML = data.price
-        }
+      let option = document.createElement("option")
+      option.value = color
+      option.innerHTML = color
+      colorSelect.appendChild(option)
+    }
 
-      console.log(data);
+    console.log(data);
 
-      let addButton = document.querySelector("addToCart")
-      addButton.addEventListener("click", function(event){
-        window.location.href = "./cart.html";
+    let addButton = document.querySelector("#addToCart")
+    addButton.addEventListener("click", function (event) {
+      
 
-      })
-    });
+
+
+    })
+  });
