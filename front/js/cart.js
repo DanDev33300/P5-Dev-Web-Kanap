273484a319742ca8c814a0fbe2a271f215bd14e1
Dan Bareille <dan.bareillegmail.com>
2022-08-25 16:23:02 +0200

    Modification du DOM cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,7 @@
 let productSavedInLocalStorage = getAllProductsFromLocalStorage()
 console.log(productSavedInLocalStorage);
 
+let itemsCart = document.querySelector("#cart__items")
 
 //tout d'abord il faut faire une boucle sur le panier de l'utilisateur pour faire un traitement par produit (en destructurant le produit).
 for (let [id, colors] of Object.entries(productSavedInLocalStorage)) {
@@ -8,38 +9,37 @@ for (let [id, colors] of Object.entries(productSavedInLocalStorage)) {
     //la deuxième boucle permets d'aller récupérer la couleur et la quantité
     for (let [color, quantity] of Object.entries(colors)) {
         console.log(color)
-        fetch("http://localhost:3000/api/products/")
+        //ce qui ta embete c'est de récupérer tous les produits à chaque boucle alors qu'il ne faut en récupérer qu'un seul
+        //avec l'id
+        fetch("http://localhost:3000/api/products/" + id)
             .then(response => response.json())
             .then(product => {
-                console.table(product);
+                // console.table(product);
 
-                let itemsCart = document.querySelector("#cart__items")
-
-                for (j = 0; j < product.length; j++) {
+                // for (j = 0; j < product.length; j++) {
                     const productCart = `
-                    <article class="cart__item" data-id="${product[j]._id}" data-color="${product[j].colors}">
+                    <article class="cart__item" data-id="${product._id}" data-color="${product.colors}">
                       <div class="cart__item__img">
-                        <img src="${product[j].imageUrl}" alt="${product[j].altTxt}"/>
+                        <img src="${product.imageUrl}" alt="${product.altTxt}"/>
                       </div>
                       <div class="cart__item__content">
-                        <div class="cart__item__content__description">${product[j].description}
-                          <h2>${product[j].name}</h2>
-                          <p>${product[j]}</p>
-                          <p>${product[j].price}€</</p>
+                        <div class="cart__item__content__description">
+                          <h2>${product.name}</h2>
+                          <p>${product.description}</p>
+                          <p>${product.price}€</</p>
                         </div>
                         <div class="cart__item_content__settings">
                           <div class="cart__item__content__settings___quantity">
                             <p>Qté : </p>
-                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantity}">
                           <div class="cart__item__content__settings__delete">
                             <p class="deleteItem">Supprimer</p>
                           </div>
                         </div>
-                      </div>      
+                      </div>
                     </article>
                     `
-                    itemsCart.innerHTML = productCart;
-                }
+                    itemsCart.innerHTML += productCart;
             })
 
 
@@ -58,14 +58,16 @@ for (let [id, colors] of Object.entries(productSavedInLocalStorage)) {
         Object.values(changedQuantitys).forEach(changedQuantity => {
             changedQuantity.addEventListener('change', function (event) {
 
-
-
-                changeQuantityToCart
+                //mettre les trois variables pour récupérer l'id, la couleur et la quantité
+                //puis les mettre en paramètre dans les parenthèses de l'appel de la fonction
+                changeQuantityToCart()
 
             })
         })
     }
 }
 
-  //À cet endroit, tu peux commencer à travailler la gestion du formulaire.
-//En mettant un écouteur d'événements au clic pour valider chaque champ.
+//   À cet endroit, tu peux commencer à travailler la gestion du formulaire.
+// En mettant un écouteur d'événements au clic pour valider chaque champ.
+
+
